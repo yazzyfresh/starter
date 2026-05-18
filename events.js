6e13155cba51514ed760d0b0fce83709ebf296c2
yazzyfresh <yazzy06ramirez@aol.com>
2026-05-18 17:38:31 -0400

    new video

--- events.js
+++ events.js
@@ -9,19 +9,19 @@ class Sales extends EventEmitter {
 
 const sales = new Sales();
 
-myEmitter.on("newSale", () => {
+sales.on("newSale", () => {
   console.log("There was a new sale!");
 });
 
-myEmitter.on("newSale", () => {
+sales.on("newSale", () => {
   console.log("Customer name: Jonas");
 });
 
-myEmitter.on("newSale", (stock) => {
+sales.on("newSale", (stock) => {
   console.log(`There are now ${stock} items left in stock.`);
 });
 
-myEmitter.emit("newSale", 9);
+sales.emit("newSale", 9);
 
 ////////////////
 
@@ -29,11 +29,18 @@ const server = http.createServer();
 
 server.on("request", (req, res) => {
   console.log("Request received!");
+  console.log(req.url); 
   res.end("Request received");
-})
-
-server.on("request", (req, res) => {
-  res.end("Another request ");
 });
 
+server.on("request", (req, res) => {
+  console.log("Another request");
+});
 
+server.on("close", () => {
+  console.log("Server closed");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Waiting for requests...");
+});
