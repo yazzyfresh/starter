7112d523b77f15ada67501043acaa2fcd58032a2
yazzyfresh <yazzy06ramirez@aol.com>
2026-05-18 17:20:47 -0400

    adding request

--- events.js
+++ events.js
@@ -1,6 +1,13 @@
 const EventEmitter = require("events");
+const http = require("http");
 
-const myEmitter = new EventEmitter();
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+
+const sales = new Sales();
 
 myEmitter.on("newSale", () => {
   console.log("There was a new sale!");
@@ -15,3 +22,18 @@ myEmitter.on("newSale", (stock) => {
 });
 
 myEmitter.emit("newSale", 9);
+
+////////////////
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  console.log("Request received!");
+  res.end("Request received");
+})
+
+server.on("request", (req, res) => {
+  res.end("Another request ");
+});
+
+
