51d825f26281c188f123727c80c5b276f8408e72
yazzyfresh <yazzy06ramirez@aol.com>
2026-05-18 12:43:31 -0400

    items left in stock

--- events.js
+++ events.js
@@ -10,4 +10,9 @@ myEmitter.on("newSale", () => {
   console.log("Customer name: Jonas");
 });
 
+
+myEmitter.on("newSale", (stock) => {
+  console.log(`There are now ${stock} items left in stock.`);
+});
+
 myEmitter.emit("newSale");
