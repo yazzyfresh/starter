e53fee06ebc3fa48aba2f62c130259166e0eb5da
yazzyfresh <yazzy06ramirez@aol.com>
2026-05-18 16:03:31 -0400

    adding stock amount

--- events.js
+++ events.js
@@ -10,9 +10,8 @@ myEmitter.on("newSale", () => {
   console.log("Customer name: Jonas");
 });
 
-
 myEmitter.on("newSale", (stock) => {
   console.log(`There are now ${stock} items left in stock.`);
 });
 
-myEmitter.emit("newSale");
+myEmitter.emit("newSale", 9);
