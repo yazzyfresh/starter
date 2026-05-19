e027827fb6dc12851065130ccbdb7ce7f0bf7a9d
yazzyfresh <yazzy06ramirez@aol.com>
2026-05-19 13:09:31 -0400

    creating a 3ed solution

--- streams.js
+++ streams.js
@@ -8,20 +8,24 @@ server.on("request", (req, res) => {
   //     res.end(data);
   // });
 
-  //Soulution 2 Streams
-  const readable = fs.createReadStream("test-file.txt");
-  readable.on("data", (chunk) => {
-    res.write(chunk);
-  });
-  readable.on("end", () => {
-    res.end();
-  });
-  readable.on("error", (err) => {
-    console.log(err);
-    res.statusCode = 500;
-    res.end("File not found");
-  });
+//   //Soulution 2 Streams
+//   const readable = fs.createReadStream("test-file.txt");
+//   readable.on("data", (chunk) => {
+//     res.write(chunk);
+//   });
+//   readable.on("end", () => {
+//     res.end();
+//   });
+//   readable.on("error", (err) => {
+//     console.log(err);
+//     res.statusCode = 500;
+//     res.end("File not found");
+//   });
+// 
+
 });
+
+
 
 server.listen(8000, "127.0.0.1", () => {
   console.log("Listening...");
