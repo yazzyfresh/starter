6f33e7e421b699846fc504a025dfa2a88ddc32d9
yazzyfresh <yazzy06ramirez@aol.com>
2026-05-19 09:56:28 -0400

    staring 2ed solution

--- streams.js
+++ streams.js
@@ -2,11 +2,15 @@ const fs  = require('fs');
 const server  = require('http').createServer();
 
 server.on('request', (req, res) => {
-    //Soulution 1
-    fs.readFile('test-file.txt', (err, data) => {
-        if (err) console.log(err);
-        res.end(data);
-    });
+    // //Soulution 1
+    // fs.readFile('test-file.txt', (err, data) => {
+    //     if (err) console.log(err);
+    //     res.end(data);
+    // });
+
+    //Soulution 2 Streams
+    const readable = fs.createReadStream('test-file.txt');
+    readable.pipe(res);
 
 });
 server.listen (8000, '127.0.0.1', () => {
