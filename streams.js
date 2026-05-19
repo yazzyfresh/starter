55d8c5f4bd45bc85927a04ffb6c919787e8a35c1
yazzyfresh <yazzy06ramirez@aol.com>
2026-05-19 12:58:06 -0400

    fixing and edditing the issues

--- streams.js
+++ streams.js
@@ -1,28 +1,28 @@
-const fs  = require('fs');
-const server  = require('http').createServer();
+const fs = require("fs");
+const server = require("http").createServer();
 
-server.on('request', (req, res) => {
-    // //Soulution 1
-    // fs.readFile('test-file.txt', (err, data) => {
-    //     if (err) console.log(err);
-    //     res.end(data);
-    // });
+server.on("request", (req, res) => {
+  // //Soulution 1
+  // fs.readFile('test-file.txt', (err, data) => {
+  //     if (err) console.log(err);
+  //     res.end(data);
+  // });
 
-    //Soulution 2 Streams
-    const readable = fs.createReadStream('test-file.txt');
-    readable.on('data', (chunk) => {
-        res.write(chunk);
-    });
-    readable.on('end', () => {
-        res.end();
-    });
-    readable.on('error', (err) => {
-        console.log(err);
-        res.statusCode = 500;
-        res.end('File not found');
-    });
+  //Soulution 2 Streams
+  const readable = fs.createReadStream("test-file.txt");
+  readable.on("data", (chunk) => {
+    res.write(chunk);
+  });
+  readable.on("end", () => {
+    res.end();
+  });
+  readable.on("error", (err) => {
+    console.log(err);
+    res.statusCode = 500;
+    res.end("File not found");
+  });
 });
 
-server.listen (8000, '127.0.0.1', () => {
-    console.log('Listening...');
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening...");
 });
